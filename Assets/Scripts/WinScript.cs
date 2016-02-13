0211cd3a571159f76869ec895fc90bcac26eed7a
Wesley Lin <wesley.lin@vanderbilt.edu>
2016-02-13 12:15:15 +0100

    added new win script and win prefab

---
+++ Assets/Scripts/WinScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.SceneManagement;
+
+public class WinScript : MonoBehaviour {
+
+    public string nextScene;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.GetComponent<OrdinaryHuman>() != null)
+        {
+            Invoke("LoadNextScene", 5f);
+        }
+    }
+
+
+    void LoadNextScene()
+    {
+        EditorSceneManager.LoadScene(nextScene);
+    }
+}
